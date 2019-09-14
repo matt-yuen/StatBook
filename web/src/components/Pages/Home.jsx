853753e1635a99ce1d1679d2700bb18bc6c3ee03
Matthew Yuen <un.matthew@gmail.com>
2019-09-14 14:20:42 -0400

    Add basic homepage and borders

--- web/src/components/Pages/Home.jsx
+++ web/src/components/Pages/Home.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 24px;
+`;
+
+const WelcomeMessage = styled.div`;
+`;
+
+const Logo = styled.div`
+`;
 
 function Home() {
   return (
-    <div>Home</div>  
+    <Header>
+      <WelcomeMessage>
+        Welcome back!
+      </WelcomeMessage>
+      <Logo>
+        StatBook
+      </Logo>
+    </Header>
   )
 }
 
--- web/src/components/Pages/Login.jsx
+++ web/src/components/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
 import FacebookLogin from 'react-facebook-login';
 import { withRouter } from 'react-router-dom';
