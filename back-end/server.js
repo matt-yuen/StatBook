c0fc3ac10a579d883bfc7eb11410e6eee5379574
Volodymyr <klymenkodp@gmail.com>
2019-09-15 00:32:34 -0400

    engine: Add basic analysis functions.

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const axios = require('axios');
+const RecommendationEngine = require('./RecommendationEngine');
+
 const app = express();
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
-    res.send('StatBook back-end');
+  res.send('StatBook back-end');
 });
 
-app.listen(8080, () =>
-  console.log('Server listening on port 8080!'),
-);
+app.post('/posts', (req, res) => {
+  let data;
+  const recommendationEngine = new RecommendationEngine();
+  recommendationEngine.processPosts(req.body).then((response) => {
+    console.log("SENDING");
+    console.log(response);
+    res.json(response);
+  });
+});
+
+app.listen(8080, () => console.log('Server listening on port 8080!'));
