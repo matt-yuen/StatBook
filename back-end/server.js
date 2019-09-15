5c558c01fc2bb2cad04134a959c9910f3101ec7a
Volodymyr <klymenkodp@gmail.com>
2019-09-15 06:38:42 -0400

    fix cors

--- back-end/server.js
+++ back-end/server.js
@@ -4,10 +4,11 @@ const RecommendationEngine = require('./RecommendationEngine');
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
-app.use(() => {
-  express.json();
+app.use(express.json());
+app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 
 app.get('/', (req, res) => {
